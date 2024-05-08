a1aae16acfb9e74d8b5dce81aa306d1b1aef28b7
Каджик Гаспарян <kj_mac@MacBook-Pro-Kadzik.local>
2024-05-08 20:33:16 +0300

    feat: настройка пре-коммитов

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,26 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import parserEslint from '@typescript-eslint/parser';
 
 export default [
-  { files: ["**/*.js", "**/*.ts"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.js', '**/*.ts'],
+    languageOptions: { sourceType: 'commonjs' },
+  },
+  {
+    languageOptions: { globals: globals.browser },
+  },
+  // parserEslint,
+  // eslintConfigPrettier,
+  // typescriptEslint,
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  // {
+  //   parser: '@typescript-eslint/parser',
+  //   plugins: ['@typescript-eslint'],
+  //   root: true,
+  // },
 ];
